dc7184e429ebe870a25cce24b9b8b9e64c893aab
Eole Rudant <eole.rudant@etu.minesparis.psl.eu>
2025-11-04 15:58:01 +0100

    bulletin

--- TP classes.py
+++ TP classes.py
@@ -15,17 +15,36 @@ class Student:
             raise ValueError("grade must be between 0 and 20")
         self.grades[topic].append(grade)
     
-    #def followed_topics():
-       # return
+    def followed_topics(self):
+       return list(self.grades.keys())
+    
+    def compute_average(self, topic: str) -> float:
+        S=0
+        if topic not in self.grades or len(self.grades[topic]) == 0 :
+            return -1
+        else :
+            for note in self.grades[topic] :
+                S+= note
+        return S/len(self.grades[topic])
+
+    def report(self):
+        report_lines =[]
+        header = f"Report for student {self.first_name} {self.last_name}"
+        report_lines.append(header)
+        report_lines.append("+===============+===============+")
+        report_lines.append("|     Topic     |    Average    |")
+        report_lines.append("+===============+===============+")
+        
+        for topic in self.followed_topics():
+            average = self.compute_average(topic)
+            report_lines.append(f"|  {topic:<13}|    {average:>6.2f}     |") # Format topic left-aligned in 13 spaces, average right-aligned in 6 spaces with 2 decimals
+            report_lines.append("+---------------+---------------+")
+        
+        return "\n".join(report_lines)
+    
+
+    
+    
         
 
-try:
-    student = Student("Achille", "Talon")
-    student.add_grade("History", 10.)
-    student.add_grade("History", 12.)
-except Exception as e:
-    print('OOPS - There is an issue in your add_grade method.')
-    print(f"Error message : {e}")
-else:
-    print('Congrats ! Your implementation works !')
 
